e91a05a01fa84458ac3bc629860f678226772076
Brigitte Warner <brigitte.warner@gmail.com>
2014-04-10 17:58:44 -0400

    change super_admin folder name

---
+++ app/controllers/super_admin/events_controller.rb
@@ -0,0 +1,8 @@
+class Admin::EventsController < ApplicationController
+#	layout 'admin'
+	def index
+	end
+
+	def new
+	end
+end
---
+++ app/controllers/super_admin/proposals_controller.rb
@@ -0,0 +1,5 @@
+class Admin::ProposalsController < ApplicationController
+#	layout 'admin'
+	def index
+	end
+end
